94978b689cce8dcf15836ffce62280d39c74afe9
olegivo22 <124501956+olegivo22@users.noreply.github.com>
2023-02-10 14:46:40 +0300

    added cube count

--- lesson1.py
+++ lesson1.py
@@ -1,10 +1,14 @@
 # Test2
 
-<<<<<<< HEAD
+
 num1 = input('Введите первое число:') #Test comment
-=======
+
 num2 = input('Введите второе число:')
 print('*******************')
 
 print(num1, num2)
+
+cub = num2 **3
+print(cub)
+
 
