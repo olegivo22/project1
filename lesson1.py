f34bb9c172e2494b87c236059d88e1088e147040
OLEG IVANOV <olegivo@yandex.ru>
2023-02-10 17:37:23 +0300

    stash 0

--- lesson1.py
+++ lesson1.py
@@ -1,15 +1,11 @@
 # Test2
 
-<<<<<<< HEAD
 num1 = input('Введите первое число:') #Test comment
-=======
 num2 = input('Введите второе число:')
 print('*******************')
 
 print(num1, num2)
 
-<<<<<<< Updated upstream
-=======
 sqrt1 = math.sqrt(num1)
 sqrt2 = math.sqrt(num2)
 print(sqrt1)
@@ -18,5 +14,4 @@ print(sqrt2)
 
 print('*************")
       
-print('Stash 1")
->>>>>>> Stashed changes
+print('Stash 0")
