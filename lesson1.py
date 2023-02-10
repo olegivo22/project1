fa4dc2a2af6a5b1878bbcda4e8b5af2e2fd68b06
OLEG IVANOV <olegivo@yandex.ru>
2023-02-10 13:55:08 +0300

    added new code sqrt count

--- lesson1.py
+++ lesson1.py
@@ -1,10 +1,13 @@
 # Test2
 
-<<<<<<< HEAD
+import math
+
 num1 = input('Введите первое число:') #Test comment
-=======
+
 num2 = input('Введите второе число:')
 print('*******************')
 
 print(num1, num2)
 
+sqrt = math.sqrt(num1)
+print(nsqrt)
