5f3f5ce077a4cf936b57a0cdd67bb4cb45dc2a53
OLEG IVANOV <olegivo@yandex.ru>
2023-02-10 17:31:49 +0300

    added stash1

--- lesson1.py
+++ lesson1.py
@@ -8,3 +8,15 @@ print('*******************')
 
 print(num1, num2)
 
+<<<<<<< Updated upstream
+=======
+sqrt1 = math.sqrt(num1)
+sqrt2 = math.sqrt(num2)
+print(sqrt1)
+print(sqrt2)
+
+
+print('*************")
+      
+print('Stash 1")
+>>>>>>> Stashed changes
